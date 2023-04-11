fb95c00544e2397cf265e175d462b9b128ed4723
MadihaAftab <madeeha.hiba123@gmail.com>
2023-04-12 03:15:51 +0500

    Another folder included

---
+++ customerProfile/myscript.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var table = document.getElementById("myTable");
+  var rows = table.getElementsByTagName("tr");
+  for (var i = 0; i < rows.length; i++) {
+    var row = rows[i];
+    row.addEventListener("click", function() {
+      var link = this.dataset.href;
+      if (link) {
+        window.location.href = "jobDetails.html";
+      }
+    });
+  }
+});
+
+
